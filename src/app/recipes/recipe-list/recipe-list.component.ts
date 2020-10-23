496aa21b2b25bf43d3a216153fa076c9081d6192
Sheena <sheenathomas003@gmail.com>
2020-10-23 11:53:57 +0530

    recipedetails

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+ import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] =  [
+  new Recipe('A test Recipe', 'This is simply a test' , 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190313-creamy-lemon-parmesan-chicken-horizontal-1553026901.png?crop=1.00xw:0.752xh;0,0.144xh&resize=980:*')
+];
 
   constructor() { }
 
